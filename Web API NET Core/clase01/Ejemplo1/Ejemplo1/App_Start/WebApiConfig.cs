d7a5a4e99bea471aa3fce33a03e291ef5f698b83
fersayago <sayago.fernando@gmail.com>
2021-07-23 12:10:58 -0300

    2021-07-23 -> Web API NET Core - clase 1: Introduccion y creacion de API

---
+++ Web API NET Core/clase01/Ejemplo1/Ejemplo1/App_Start/WebApiConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace Ejemplo1
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Configuración y servicios de API web
+            // removemos el XML de los formatos para que devuelva JSON
+            config.Formatters.Remove(GlobalConfiguration.Configuration.Formatters.XmlFormatter)
+            // Rutas de API web
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
